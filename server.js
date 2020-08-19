e6eb5e4c282add2b96a0e18ede74d9103515f1d1
NextLevelDeveloper <nldnathaniel@gmail.com>
2020-08-19 13:33:01 -0400

    mongo db working and gitignore added

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express')
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const expressEjsLayouts = require('express-ejs-layouts')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -10,6 +14,12 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', (error) => {console.error(error)})
+db.once('open', () => {console.log('Connected to Mongoose')})
 
 app.use('/', indexRouter)
 
