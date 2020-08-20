3ab4027f7e57aaac2ddf6571e9edcd92e462e485
NextLevelDeveloper <nldnathaniel@gmail.com>
2020-08-20 08:23:00 -0400

    first async function :P

--- routes/authors.js
+++ routes/authors.js
@@ -13,22 +13,23 @@ router.get('/new', (req, res)=>{
 })
 
 //CREATE new author route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
+    
 
-    author.save((err, newAuthor)=>{
-        if(err){
-            res.render('authors/new', {
-                author: author,
-                errorMessage: 'Error creating Author'
-            })
-        }else{
-            // res.redirect('authors/${newAuthor.id}')
-            res.redirect('authors')
-        }
-    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors/${newAuthor.id}')
+        res.redirect('authors')
+    } catch (error) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+
 })
 
 module.exports = router
