bd2cba1de8ed692a1ccb7da28fd04ac4bad0c1fe
NextLevelDeveloper <nldnathaniel@gmail.com>
2020-08-19 21:19:52 -0400

    working state right after finishing the new author input

--- routes/authors.js
+++ routes/authors.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Author = require('../models/author')
 const router = express.Router()
 
 //All authors route
@@ -8,12 +9,26 @@ router.get('/', (req, res)=>{
 
 //NEW authors route (form)
 router.get('/new', (req, res)=>{
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 //CREATE new author route
 router.post('/', (req, res) => {
-    res.send('Create')
+    const author = new Author({
+        name: req.body.name
+    })
+
+    author.save((err, newAuthor)=>{
+        if(err){
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        }else{
+            // res.redirect('authors/${newAuthor.id}')
+            res.redirect('authors')
+        }
+    })
 })
 
 module.exports = router
