d2e2a0ba56964e58b38d8416c8ce228ee1483f4f
NextLevelDeveloper <nldnathaniel@gmail.com>
2020-08-27 02:36:03 -0400

    added author edit, update, delete to be working, deleteing an author deletes all books associated with them

--- routes/authors.js
+++ routes/authors.js
@@ -33,15 +33,13 @@ router.post('/', async (req, res) => {
     })
     try {
         const newAuthor = await author.save()
-        // res.redirect('authors/${newAuthor.id}')
-        res.redirect('authors')
+        res.redirect(`authors/${newAuthor.id}`)
     } catch (error) {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating Author'
         })
     }
-
 })
 
 //show
@@ -50,18 +48,50 @@ router.get('/:id', (req, res) => {
 })
 
 //edit
-router.get('/:id/edit', (req, res) => {
-    res.send('Edit Author ' + req.params.id)
+router.get('/:id/edit', async (req, res) => {
+    try{
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', { author: author })
+    }catch{
+        res.redirect('/authors')
+    }
+    
 })
 
 //update
-router.put('/:id', (req, res) => {
-    res.send('Update Author ' + req.params.id)
+router.put('/:id', async (req, res) => {
+    let author 
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch (error) {
+        if(author == null){
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+    }
 })
 
 //delete
-router.delete('/:id', (req, res) => {
-    res.send('Delete Author ' + req.params.id)
+router.delete('/:id', async (req, res) => {
+    let author 
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors')
+    } catch (error) {
+        if(author == null){
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
+    }
 })
 
 module.exports = router
